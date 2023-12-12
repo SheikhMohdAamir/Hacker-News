73143d3174557ec93a1cd9303e7c8cc780f16847
Sheikh Mohd Aamir <129277096+SheikhMohdAamir@users.noreply.github.com>
2023-12-12 15:53:45 +0530

    Fetching News Data Done

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,8 +1,33 @@
+import { useEffect } from 'react'
+import axios from 'axios'
+import Spinner from 'react-bootstrap/Spinner';
 
 const News = () => {
+    let loading = true;
+    let API = 'http://hn.algolia.com/api/v1/search?query=html'
+
+    const fetchNewsData = async(url) => {
+        try{
+            const response = await axios.get(url)
+            const data = response.data.hits
+            console.log(data)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+    useEffect(()=>{
+        fetchNewsData(API)
+    },[])
+
+    if(loading===true) return <Spinner animation="border" variant="dark" />
+    
   return (
-    <h1>News</h1>
+    <>
+    {!loading && ''}
+    </>
   )
 }
 
-export default News
+export default News;
