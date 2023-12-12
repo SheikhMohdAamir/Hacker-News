fbcc3d331e4a7e4f504b7111672f6feb132c99fa
Sheikh Mohd Aamir <129277096+SheikhMohdAamir@users.noreply.github.com>
2023-12-12 15:09:04 +0530

    Folder Structure Done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Search from "./Components/Search";
+import Pagination from "./Components/Pagination";
+import News from "./Components/News";
+
+function App() {
+  return (
+    <>
+      <Search />
+      <Pagination />
+      <News />
+    </>
+  );
+}
+
+export default App;
